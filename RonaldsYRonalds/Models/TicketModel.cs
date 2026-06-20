fb155ae55a2ee99249da9e5dcb99ef06b2aef4a1
Michael Neugebauer <mtneugebauer1998@outlook.com>
2026-06-20 17:42:15 -0500

    Added a model for the tickets

---
+++ RonaldsYRonalds/Models/TicketModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RonaldsYRonalds.Models
+{
+    public class TicketModel
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public string UserName { get; set; }
+        public TicketStatus Status { get; set; } = TicketStatus.Submitted;
+        [StringLength(17, MinimumLength = 17)]
+        public string Vin { get; set; }
+        public string IncidentDescription { get; set; }
+    }
+}
