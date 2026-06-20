27c4a47a4bf1027bf1d5f5c22cd016f4b349981a
Michael Neugebauer <mtneugebauer1998@outlook.com>
2026-06-20 14:42:19 -0500

    Finally got view code to scaffold!

---
+++ RonaldsYRonalds/Controllers/DevController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RonaldsYRonalds.Controllers
+{
+    public class DevController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public DevController(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> MakeAdmin(string email)
+        {
+            var user = await _userManager.FindByEmailAsync(email);
+
+            if (user != null)
+            {
+                await _userManager.AddToRoleAsync(user, "Admin");
+            }
+
+            return Content("Done");
+        }
+    }
+}
