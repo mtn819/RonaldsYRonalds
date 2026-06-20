35c9053f1affeba63e34a9ec11c6c7ab45cc4969
Michael Neugebauer <mtneugebauer1998@outlook.com>
2026-06-20 17:58:44 -0500

    Cleared unnecessary?? (please be unnecessary) context.

--- RonaldsYRonalds/Data/RonaldsYRonaldsContext.cs
+++
@@ -1,6 +0,0 @@
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore;
-
-public class RonaldsYRonaldsContext(DbContextOptions<RonaldsYRonaldsContext> options) : IdentityDbContext<RonaldsYRonalds.Data.ApplicationUser>(options)
-{
-}
