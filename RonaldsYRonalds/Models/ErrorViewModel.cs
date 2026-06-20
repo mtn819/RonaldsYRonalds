cfb58fd6ddeabccdf1ce7d787e8450e322fb0f3d
mtneu <mtneu@POOP>
2026-06-20 11:40:16 -0500

    Add project files.

---
+++ RonaldsYRonalds/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RonaldsYRonalds.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
