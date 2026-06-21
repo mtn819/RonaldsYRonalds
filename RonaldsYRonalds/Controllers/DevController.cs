2f96f6a45a093a4fc94280d4b70088bbe1896052
Michael Neugebauer <mtneugebauer1998@outlook.com>
2026-06-21 08:43:27 -0500

    Added an endpoint for creating admins for when I deploy.

---
+++ RonaldsYRonalds/Controllers/DevController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RonaldsYRonalds.Controllers
+{
+    public class DevController(UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager) : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager = userManager;
+        private readonly RoleManager<IdentityRole> _roleManager = roleManager;
+
+        [HttpGet]
+        public async Task<IActionResult> MakeAdmin(string username)
+        {
+            // create role
+            if (!await roleManager.RoleExistsAsync("Admin"))
+                await roleManager.CreateAsync(new IdentityRole("Admin"));
+
+            // pick user
+            var user = await userManager.FindByEmailAsync(username);
+
+            if (user != null)
+            {
+                await userManager.AddToRoleAsync(user, "Admin");
+            }
+
+            return Ok($"{username} is admin");
+        }
+    }
+}
