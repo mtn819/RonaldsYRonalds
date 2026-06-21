e706cf6ea1a27fb15951f0517ef10845385f9888
Michael Neugebauer <mtneugebauer1998@outlook.com>
2026-06-21 06:42:26 -0500

    Removed an option from Ticket Status Enum.

---
+++ RonaldsYRonalds/Data/Migrations/20260621113851_RemoveEnumOption.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RonaldsYRonalds.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveEnumOption : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
--- RonaldsYRonalds/Models/TicketStatus.cs
+++ RonaldsYRonalds/Models/TicketStatus.cs
@@ -4,7 +4,6 @@
     {
         Submitted,
         Rejected,
-        Processing,
         Fulfilled
     }
 }
