94e4d9b033106232d3fafddffceda4ae44ea0845
Michael Neugebauer <mtneugebauer1998@outlook.com>
2026-06-20 23:16:56 -0500

    Added CreatedAt Stamps To Tickets

--- RonaldsYRonalds/Models/TicketModel.cs
+++ RonaldsYRonalds/Models/TicketModel.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace RonaldsYRonalds.Models
 {
@@ -8,7 +9,9 @@ namespace RonaldsYRonalds.Models
         public string UserName { get; set; }
         public TicketStatus Status { get; set; } = TicketStatus.Submitted;
         //[StringLength(17, MinimumLength = 17)]
+        [Display(Name="Vehicle Identification Number (VIN)")]
         public string Vin { get; set; }
         public string IncidentDescription { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     }
 }
