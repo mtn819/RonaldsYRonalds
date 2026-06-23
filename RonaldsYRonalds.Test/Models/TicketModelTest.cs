ef84d2cb80b002a4269af65e72d83f840fbd6c58
Michael Neugebauer <mtneugebauer1998@outlook.com>
2026-06-22 19:22:04 -0500

    Ticket Model Tests 8)

---
+++ RonaldsYRonalds.Test/Models/TicketModelTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using RonaldsYRonalds.Models;
+using Xunit;
+
+namespace RonaldsYRonalds.Test.Models
+{
+    public class TicketModelTest
+    {
+        [Fact]
+        public void DefaultTicket()
+        {
+            var before = DateTime.UtcNow;
+            var ticket = new TicketModel();
+            var after = DateTime.UtcNow;
+
+            Assert.InRange(ticket.CreatedAt, before, after);
+            Assert.Equal(TicketStatus.Submitted, ticket.Status);
+        }
+
+        [Fact]
+        public void SetTicket()
+        {
+            var ticket = new TicketModel
+            {
+                Vin = "12345678901234567",
+                IncidentDescription = "AKA The Claim",
+                UserName = "UserName@Email.Com",
+                Status = TicketStatus.Rejected
+            };
+
+            Assert.Equal("12345678901234567", ticket.Vin);
+            Assert.Equal("AKA The Claim", ticket.IncidentDescription);
+            Assert.Equal("UserName@Email.Com", ticket.UserName);
+            Assert.Equal(TicketStatus.Rejected, ticket.Status);
+        }
+    }
+}
